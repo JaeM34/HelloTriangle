0d8139e60f316dc1b8211dac5b659ea3b8765bbf
JustinM3434 <justinmolina34@gmail.com>
2022-11-28 12:57:42 -0800

    Setup window flags

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 
 class HelloTriangleApplication {
 public:
+    GLFWwindow* window;
+
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
     void run() {
         initWindow();
         initVulkan();
@@ -15,14 +23,27 @@ public:
     }
 
 private:
+    void initWindow() {
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+        //Disabling resizeable
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        //Setting size of window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
     void initVulkan() {
 
     }
     void mainLoop() {
-
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
     void cleanup(){
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
